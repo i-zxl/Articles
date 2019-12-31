4550c41cebd6fb2697dd275d58b9d9cd323c9747
zhaoxinglai <zhaoxinglai@yidian-inc.com>
2019-12-31 17:11:47 +0800

    revert add a5f5afa239e7ab6e56a92197754d6ff82b16151a

--- Nodejs/Module/index.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-fs.readFile('./module.js', 'utf-8', function(err, data) {
-  let context = `(function(exports,require, module, __filename, __dirname) { \n ${data} \n})`
-  let m = eval(context)
-  console.log(String(m))
-})
